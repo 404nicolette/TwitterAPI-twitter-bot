0805a0c489fe1d7d3bc387997b19038594c2640a
404nicolette <123152389+404nicolette@users.noreply.github.com>
2023-09-15 18:41:44 +0100

    first commit

---
+++ twt_bot.py
@@ -0,0 +1,42 @@
+import tweepy
+import time
+import random
+from dotenv import load_dotenv
+import os
+
+print(os.getenv('PATH'))
+
+load_dotenv()  # environment variable
+
+# get the api keys and tokens
+API_KEY = os.getenv("API_KEY")
+API_SECRET = os.getenv("API_SECRET")
+BEARER_TOKEN = os.getenv(r"BEARER_TOKEN")
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+ACCESS_TOKEN_SECRET_KEY = os.getenv("ACCESS_TOKEN_SECRET_KEY")
+
+client = tweepy.Client(BEARER_TOKEN, API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET_KEY)
+auth = tweepy.OAuth1UserHandler(API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET_KEY)
+api = tweepy.API(auth)
+
+
+# client.create_tweet(text="Test one")
+
+def song_bot():
+    with open('lyrics.txt', 'r') as f:
+        verses = f.read().split("\n\n")  # split according to empty lines
+
+    random.shuffle(verses)
+    tweeted_verse = set()  # creates a set to keep track of tweeted verses to avoid repetition
+
+    for verse in verses:
+        verse = verse.strip()  # remove white space
+
+        if verse and (verse not in tweeted_verse):  # two condition: must be a verse and must not be in tweeted_verse
+            client.create_tweet(text=verse)
+            tweeted_verse.add(verse)  # will store to tweeted_verse now
+
+            time.sleep(5*60*60)
+
+
+song_bot()
